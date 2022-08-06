225cf66b376193ffd46451e90f2202ed66314c50
Kyle Parrish <kfparri@gmail.com>
2022-08-06 09:15:57 -0500

    Added simon and upload/download

---
+++ upload.py
@@ -0,0 +1,74 @@
+# Imports
+from distutils.command.upload import upload
+from turtle import up
+import pygame, sys, threading
+
+pygame.init()
+
+# Screen and font
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Upload")
+
+FONT = pygame.font.SysFont("Roboto", 100)
+
+# Clock
+CLOCK = pygame.time.Clock()
+
+# Work
+WORK = 100000000
+
+# Loading BG
+LOADING_BG = pygame.image.load('Loading Bar Background.png')
+LOADING_BG_RECT = LOADING_BG.get_rect(center=(640, 360))
+
+# Loading Bar and Variables
+LOADING_BAR = pygame.image.load('Loading Bar.png')
+loading_bar_rect = LOADING_BAR.get_rect(midleft=(280, 360))
+loading_finished = False
+loading_progress = 0
+loading_bar_width = 8
+
+def doWork():
+    # do some math WORK amount of times
+    global loading_finished, loading_progress
+
+    for i in range(WORK):
+        math_equation = 523687 / 23458 * 892345
+        loading_progress = i
+
+    loading_finished = True
+
+# Finished text
+finished = FONT.render("Task Complete!", True, "white")
+finished_rect = finished.get_rect(center=(640,360))
+
+# upload text
+uploadText = FONT.render("Uploading...", True, "white")
+uploadText_rect = uploadText.get_rect(center=(640, 230))
+
+# Thread
+threading.Thread(target=doWork).start()
+# Game Loop
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.fill("#0d0e2e")
+
+    if not loading_finished:
+        loading_bar_width = loading_progress / WORK * 720
+        
+        loading_bar = pygame.transform.scale(LOADING_BAR, (int(loading_bar_width), 150))
+        loading_bar_rect = loading_bar.get_rect(midleft=(280, 360))
+
+        screen.blit(LOADING_BG, LOADING_BG_RECT)
+        screen.blit(uploadText, uploadText_rect)
+        screen.blit(loading_bar, loading_bar_rect)
+    else:
+        screen.blit(finished, finished_rect)
+
+    
+    pygame.display.update()
+    CLOCK.tick(60)
